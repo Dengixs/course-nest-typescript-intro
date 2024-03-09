a19007b08c009030319644d0a585cfba0e63df0a
Brayan Cahuana <brayan.janco@gmail.com>
2024-03-08 20:30:57 -0400

    first commit

---
+++ src/bases/01-types.ts
@@ -0,0 +1,9 @@
+
+export let name: string = 'Fernando';
+export const age: number = 31;
+export const isValid: boolean = true;
+
+name = 'Melissa';
+
+export const templateString = `Hola, mi nombre es ${ name } y mi edad es ${ age }`;
+
